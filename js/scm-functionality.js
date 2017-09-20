cc79411bed6bbc341fe25ede36b80166cbdacd6b
Kyle Kerley <kyle.l.kerley@gmail.com>
2017-09-20 14:15:37 -0600

    Initial commit

---
+++ js/scm-functionality.js
@@ -0,0 +1,26 @@
+jQuery(document).ready(function($){
+   var $scm_menu = $('.scm-sidebar-child-menu');
+   var $icon_open = $scm_menu.data('icon-menu-open');
+   var $disable_js = $scm_menu.data('disable-js');
+   // console.log($disable_js);
+
+   if($disable_js !== "on"){
+       if($icon_open === ''){
+           $icon_open = 'fa-chevron-up';
+       }
+       var $icon_closed = $scm_menu.data('icon-menu-closed');
+       if($icon_closed === ''){
+           $icon_closed = 'fa-chevron-down';
+       }
+       var combined_classes = $icon_open + ' ' + $icon_closed;
+
+       $scm_menu.find('li.parent-item.top-level, li.parent-item.active-trail, li.parent-item.current-page').append('<i class="fa ' + $icon_open + '"></i>');
+       $scm_menu.find('li.parent-item:not(.top-level):not(.active-trail):not(.current-page)').append('<i class="fa ' + $icon_closed + '"></i>');
+
+       $('body').on('click', '.scm-sidebar-child-menu .parent-item > i.fa', function(){
+           var $this = $(this);
+           $this.toggleClass(combined_classes);
+           $this.parent('.parent-item').find('> .child-menu').slideToggle('fast');
+       });
+   }
+});
